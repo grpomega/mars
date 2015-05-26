36c4448f19f881e7172c52a717b44baa9d1e328a
Thomas Schoebel-Theuer <tst@1und1.de>
2015-05-26 13:45:35 +0200

    infra: replace sys_mkdir() by vfs_mkdir()

--- kernel/provisionary_wrapper.h
+++ kernel/provisionary_wrapper.h
@@ -37,5 +37,9 @@
 int _provisionary_wrapper_to_vfs_symlink(const char __user *oldname,
 					 const char __user *newname);
 
+int _provisionary_wrapper_to_vfs_mkdir(const char __user *pathname,
+				       int mode);
+
+
 #endif
 #endif
